e4de821982fe24d77b91aeb0a4072152b405539f
lUCIANOGFORTES02 <lucianogfortes@gmail.com>
2025-09-13 19:25:07 -0300

    fixing the application build

--- Frontend_Suprimento/src/routes.tsx
+++ Frontend_Suprimento/src/routes.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route } from "react-router-dom";
 import UploadPage from "./pages/upload";
 import ReviewPage from "./pages/review";
 
