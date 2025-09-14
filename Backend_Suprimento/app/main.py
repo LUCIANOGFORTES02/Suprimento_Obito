2cfe66d4a855fca0fabaf38e8be505f71620efa7
lUCIANOGFORTES02 <lucianogfortes@gmail.com>
2025-09-13 23:17:18 -0300

    feat: update cors

--- Backend_Suprimento/app/main.py
+++ Backend_Suprimento/app/main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 origins = [
     "http://localhost:5173",
     "http://localhost:8000",
-    "https://suprimento-obito-i3xf.vercel.app/",
+    "https://suprimento-obito.vercel.app/",
 ]
 
 app.add_middleware(
