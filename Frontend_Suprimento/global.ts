e6e6c293ad2155075a23e8db82ff0ed024c5f6d3
lUCIANOGFORTES02 <lucianogfortes@gmail.com>
2025-10-25 19:22:04 -0300

    New backend deploy url

--- Frontend_Suprimento/global.ts
+++ Frontend_Suprimento/global.ts
@@ -1,1 +1,2 @@
-export const baseApiUrl = "https://suprimentoobito-production.up.railway.app/";
+// export const baseApiUrl = "https://suprimentoobito-production.up.railway.app/";
+export const baseApiUrl = "https://suprimento-obito.onrender.com";
